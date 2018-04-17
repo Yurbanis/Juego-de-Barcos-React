b05b3cf13d2e2540261819616cfdf473d57c9150
Artur Ziaev <aziaev@gmail.com>
2018-04-17 22:56:22 +0300

    Algorythm now puts puts ships

--- src/app/helpers/battleship.js
+++ src/app/helpers/battleship.js
@@ -4,7 +4,12 @@ export const getBattleField = () => {
   let battleField = [];
 
   // Iterating fleet and getStartCoordinates for ship
-  let positions = generateShipPositions();
+  let positions = [];
+
+  for (let i = 0; i < shipTypes.length; i++) {
+    let newPositions = generateShipPositions(shipTypes[i], positions);
+    positions.push(...newPositions);
+  }
 
   for (let y = 0; y < 10; y++) {
     let row = [];
@@ -13,44 +18,51 @@ export const getBattleField = () => {
         xPos: x,
         yPos: y
       };
-      if (isCellOccupied(positions, currentPosition)) {
-        row.push('Х')
+      if (arePositionsOccupied(positions, [currentPosition])) {
+        row.push('Х');
       } else row.push('.');
     }
     battleField.push(row)
   }
-
+  console.log(`------------------------------`);
   return battleField;
 };
 
-const generateShipPositions = () => {
-  let positions = [];
-  for (let n = 0; positions.length < shipTypes.length; n++) {
-    console.log(`putting an ${shipTypes[n].name}`);
-    let shipPosition = getRandomCoordinate();
-    if (!isCellOccupied(positions, shipPosition)) {
-      // выбрать рандомное направление и воткнуть корабль
+const generateShipPositions = (ship, positions) => {
+  let shipStartPosition = null;
+  let i = 0;
+  while (shipStartPosition === null) {
+    shipStartPosition = getRandomCoordinate();
+    if (!arePositionsOccupied(positions, [shipStartPosition])) {
       let directions = getDirections();
-      console.log(`directions ${getDirections()}`);
-      positions.push(shipPosition);
-      // for (let i = 0; i < directions; i++) {
-      //   switch (directions[i]){
-      //     case 0:
-      //       let tryToNorth(positions, shipPosition);
-      //   }
-      // }
+      for (let i = 0; i < directions.length; i++) {
+        let tempCoordinates = tryDirections(directions[i], ship, shipStartPosition, positions);
+        if (tempCoordinates) {
+          for (let l = 0; l < tempCoordinates.length; l++) {
+            positions.push(tempCoordinates[l]);
+            positions.push(...refillNeighbourCells(tempCoordinates[l]))
+          }
+          return positions;
+        }
+      }
+    } else {
+      shipStartPosition = null;
     }
   }
-  console.log(JSON.stringify(positions));
+
   return positions;
 };
 
-const isCellOccupied = (cells, position) => {
+const arePositionsOccupied = (cells, positions) => {
   for (let i = 0; i < cells.length; i++) {
-    if (cells[i].xPos === position.xPos && cells[i].yPos === position.yPos) {
-      return true;
+    for (let j = 0; j < positions.length; j++) {
+      if (cells[i].xPos === positions[j].xPos && cells[i].yPos === positions[j].yPos) {
+        return true;
+      }
     }
   }
+  console.log(`arePositionsOccupied where to check = `, JSON.stringify(cells));
+  console.log(`arePositionsOccupied what to check = `, JSON.stringify(positions));
   return false;
 };
 
@@ -63,7 +75,108 @@ const getDirections = () => {
       directions.push(number);
     }
   }
-  console.log(`randomDirections = `, directions)
+  return directions;
+};
+
+// try to place ship to map
+function tryDirections(i, ship, shipPosition, positions) {
+  let tempCoordinates = [];
+  for (let l = 0; l < ship.size; l++) {
+    let position = {};
+    let newPos = 0;
+    switch (i) {
+      case 0:
+        newPos = shipPosition.yPos + l;
+        if (newPos > 9) {
+          console.log(`break on ${ship.name}`, newPos);
+          return null;
+        }
+        position = {
+          xPos: shipPosition.xPos,
+          yPos: newPos
+        };
+        tempCoordinates.push(position);
+        break;
+      case 1:
+        newPos = shipPosition.xPos + l;
+        if (newPos > 9) {
+          console.log(`break on ${ship.name}`, newPos);
+          return null;
+        }
+        position = {
+          xPos: newPos,
+          yPos: shipPosition.yPos,
+        };
+        tempCoordinates.push(position);
+        break;
+      case 2:
+        newPos = shipPosition.yPos - l;
+        if (newPos < 0) {
+          console.log(`break on ${ship.name}`, newPos);
+          return null;
+        }
+        position = {
+          xPos: shipPosition.xPos,
+          yPos: newPos,
+        };
+        tempCoordinates.push(position);
+        break;
+      case 3:
+        newPos = shipPosition.xPos - l;
+        if (newPos < 0) {
+          console.log(`break on ${ship.name}`, newPos);
+          return null;
+        }
+        position = {
+          xPos: newPos,
+          yPos: shipPosition.yPos,
+        };
+        tempCoordinates.push(position);
+        break;
+      default:
+        break;
+    }
+  }
+  if (arePositionsOccupied(positions, tempCoordinates)) {
+    return null;
+  } else {
+    console.log(`ready to place for ${ship.name} = ${JSON.stringify(tempCoordinates)}`);
+  }
+}
+
+// returns positions with neighbour marked
+const refillNeighbourCells = (position) => {
+  let result = [];
+  for (let i = 0; i < 4; i++) {
+    switch (i) {
+      case 0:
+        result.push({
+          xPos: position.xPos,
+          yPos: position.yPos + 1
+        });
+        break;
+
+      case 1:
+        result.push({
+          xPos: position.xPos + 1,
+          yPos: position.yPos
+        });
+        break;
+      case 2:
+        result.push({
+          xPos: position.xPos,
+          yPos: position.yPos - 1
+        });
+        break;
+      case 3:
+        result.push({
+          xPos: position.xPos - 1,
+          yPos: position.yPos
+        });
+        break;
+    }
+  }
+  return result;
 };
 
 const getRandomCoordinate = () => {
@@ -78,4 +191,5 @@ const getRandomCoordinate = () => {
 const getRandomNumber = (max) => {
   return Math.floor(Math.random() * max)
 };
+
 
